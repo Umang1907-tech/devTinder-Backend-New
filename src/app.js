c9e03b5791769b6a8c0407173e500f0663a13df9
Umang Nikhare <unikhare99@gmail.com>
2025-03-20 23:56:58 +0530

    add user route for check query and params in route

--- src/app.js
+++ src/app.js
@@ -7,8 +7,18 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello, Express.js!');
+app.get("/", (req, res) => {
+  res.send("Hello, Express.js!");
+});
+
+app.get("/user", (req, res) => {
+  const { userId, password } = req.query;
+
+  if (!userId || !password) {
+    return res.status(400).json({ error: "Missing userId or password" });
+  }
+
+  res.json({ userId, password });
 });
 
 app.listen(port, () => {
